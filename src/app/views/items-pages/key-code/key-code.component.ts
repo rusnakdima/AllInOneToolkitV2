59c1944cb5b0a04bb6dd6d4d92b436cac4e729ac
Dmitriy Rusnak <rusnakdima03@gmail.com>
2025-05-02 00:42:00 +0500

    Added virtual keyboard for page key code

--- src/app/views/items-pages/key-code/key-code.component.ts
+++ src/app/views/items-pages/key-code/key-code.component.ts
@@ -2,10 +2,22 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
+/* materials */
+import { MatIconModule } from "@angular/material/icon";
+
+/* components */
+import { KeyboardComponent } from "../../components/keyboard/keyboard.component";
+
+interface KeyboardEvent {
+  key: string;
+  code: string;
+  keyCode: number;
+}
+
 @Component({
   selector: "app-key-code",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, KeyboardComponent],
   templateUrl: "./key-code.component.html",
 })
 export class KeyCodeComponent {
@@ -15,6 +27,7 @@ export class KeyCodeComponent {
   key: string = "";
   code: string = "";
   isShow: boolean = false;
+  isShowKeyboard: boolean = false;
 
   ngOnInit(): void {
     document.addEventListener("keydown", (event: any) => {
@@ -23,5 +36,12 @@ export class KeyCodeComponent {
       this.key = event.key;
       this.code = event.code;
     });
+  }
+
+  handleVirtualKeyPress(event: KeyboardEvent): void {
+    this.isShow = true;
+    this.keyCode = event.keyCode;
+    this.key = event.key;
+    this.code = event.code;
   }
 }
