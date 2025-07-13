fd734f3f37e4945acae2e9867eeaefada31f530e
Dmitriy303 <rusnakdima03@gmail.com>
2025-07-13 13:18:57 +0500

    Returned methods in helper

--- src/app/helpers/common.ts
+++ src/app/helpers/common.ts
@@ -1,4 +1,16 @@
 export class Common {
+  static isJson(data: Object): boolean {
+    return typeof data === "object";
+  }
+  static isJsonAsString(data: string): boolean {
+    try {
+      const parsed = JSON.parse(data);
+      return typeof parsed === "object" && parsed !== null;
+    } catch (e) {
+      return false;
+    }
+  }
+
   static truncateString(str: string, length: number = 25): string {
     if (str) {
       const endIndex: number = length;
@@ -6,31 +18,31 @@ export class Common {
         return str;
       }
 
-      return str.slice(0, endIndex) + '...';
+      return str.slice(0, endIndex) + "...";
     }
-    return '';
+    return "";
   }
 
   static formatTime(date: Date | string): string {
-    if (typeof date === 'string') {
+    if (typeof date === "string") {
       date = new Date(date);
     }
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
     return `${hours}:${minutes}`;
   }
 
   static formatLocaleDate(date: Date | string): string {
-    if (typeof date === 'string' && date == '') {
+    if (typeof date === "string" && date == "") {
       date = new Date();
     }
-    if (typeof date === 'string') {
+    if (typeof date === "string") {
       date = new Date(date);
     }
     return date.toLocaleDateString(undefined, {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
+      year: "numeric",
+      month: "short",
+      day: "numeric",
     });
   }
 
@@ -62,7 +74,7 @@ export class Common {
     if (dateRec < twoDaysAgo) {
       return this.formatLocaleDate(dateRec);
     }
-    return '';
+    return "";
   }
 
   static formatTimeAgo(date: string | number | Date): string {
@@ -74,39 +86,39 @@ export class Common {
 
       const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
       if (years >= 1) {
-        return `${years} year${years > 1 ? 's' : ''} ago`;
+        return `${years} year${years > 1 ? "s" : ""} ago`;
       }
 
       const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
       const days = Math.floor(
-        (diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+        (diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24),
       );
       if (months >= 1) {
-        return `${months} month${months > 1 ? 's' : ''}${
-          days > 0 ? ` ${days} day${days > 1 ? 's' : ''}` : ''
+        return `${months} month${months > 1 ? "s" : ""}${
+          days > 0 ? ` ${days} day${days > 1 ? "s" : ""}` : ""
         } ago`;
       }
 
       const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
-        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
       );
       const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((diff % (1000 * 60)) / 1000);
       if (totalDays >= 1) {
-        return `${totalDays} day${totalDays > 1 ? 's' : ''} ${hours
+        return `${totalDays} day${totalDays > 1 ? "s" : ""} ${hours
           .toString()
-          .padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
+          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
           .toString()
-          .padStart(2, '0')} ago`;
+          .padStart(2, "0")} ago`;
       }
 
-      return `${hours.toString().padStart(2, '0')}:${minutes
+      return `${hours.toString().padStart(2, "0")}:${minutes
         .toString()
-        .padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ago`;
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")} ago`;
     }
 
-    return '';
+    return "";
   }
 
   static formatTimeIn(date: string | number | Date): string {
@@ -118,39 +130,39 @@ export class Common {
 
       const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
       if (years >= 1) {
-        return `in ${years} year${years > 1 ? 's' : ''}`;
+        return `in ${years} year${years > 1 ? "s" : ""}`;
       }
 
       const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
       const days = Math.floor(
-        (diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+        (diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24),
       );
       if (months >= 1) {
-        return `in ${months} month${months > 1 ? 's' : ''}${
-          days > 0 ? ` ${days} day${days > 1 ? 's' : ''}` : ''
+        return `in ${months} month${months > 1 ? "s" : ""}${
+          days > 0 ? ` ${days} day${days > 1 ? "s" : ""}` : ""
         }`;
       }
 
       const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
-        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
       );
       const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((diff % (1000 * 60)) / 1000);
       if (totalDays >= 1) {
-        return `in ${totalDays} day${totalDays > 1 ? 's' : ''} ${hours
+        return `in ${totalDays} day${totalDays > 1 ? "s" : ""} ${hours
           .toString()
-          .padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
+          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
           .toString()
-          .padStart(2, '0')}`;
+          .padStart(2, "0")}`;
       }
 
-      return `in ${hours.toString().padStart(2, '0')}:${minutes
+      return `in ${hours.toString().padStart(2, "0")}:${minutes
         .toString()
-        .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
     }
 
-    return '';
+    return "";
   }
 
   static formatTimeLeft(date: string | number | Date): string {
@@ -162,38 +174,38 @@ export class Common {
 
       const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
       if (years >= 1) {
-        return `${years} year${years > 1 ? 's' : ''} left`;
+        return `${years} year${years > 1 ? "s" : ""} left`;
       }
 
       const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
       const days = Math.floor(
-        (diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+        (diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24),
       );
       if (months >= 1) {
-        return `${months} month${months > 1 ? 's' : ''}${
-          days > 0 ? ` ${days} day${days > 1 ? 's' : ''}` : ''
+        return `${months} month${months > 1 ? "s" : ""}${
+          days > 0 ? ` ${days} day${days > 1 ? "s" : ""}` : ""
         } left`;
       }
 
       const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
-        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
       );
       const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((diff % (1000 * 60)) / 1000);
       if (totalDays >= 1) {
-        return `${totalDays} day${totalDays > 1 ? 's' : ''} ${hours
+        return `${totalDays} day${totalDays > 1 ? "s" : ""} ${hours
           .toString()
-          .padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
+          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
           .toString()
-          .padStart(2, '0')} left`;
+          .padStart(2, "0")} left`;
       }
 
-      return `${hours.toString().padStart(2, '0')}:${minutes
+      return `${hours.toString().padStart(2, "0")}:${minutes
         .toString()
-        .padStart(2, '0')}:${seconds.toString().padStart(2, '0')} left`;
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")} left`;
     }
 
-    return '';
+    return "";
   }
 }
