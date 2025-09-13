6ad9b6f8034e892c5c55d29babf5f5c51f8b5f57
Dmitriy303 <rusnakdima03@gmail.com>
2025-09-14 02:41:01 +0500

    Changed logic main actity

--- src-tauri/gen/android/app/src/main/java/com/tcs/allinonetoolkitv2/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/tcs/allinonetoolkitv2/MainActivity.kt
@@ -1,3 +1,9 @@
 package com.tcs.allinonetoolkitv2
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+}
