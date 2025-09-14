df6eaf67d677c7b2dff8a2a881e2f3bad9df4e48
Dmitriy303 <rusnakdima03@gmail.com>
2025-09-14 16:58:06 +0500

    v0.4.4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  version: "0.4.3",
+  version: "0.4.4",
   gitRepoName: "AllInOneToolkitV2",
   nameProduct: "All In One Toolkit v2",
   yearCreate: 2024,
